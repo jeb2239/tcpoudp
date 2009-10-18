a66ad9158e9194dcdba9fd98a09e3265f166e93e
chinmay.gaikwad <chinmay.gaikwad@469fafa2-ad12-11de-a118-c7387b1e4f66>
2009-10-18 20:34:27 +0000

    nomsg

--- trunk/toumain.cpp
+++ trunk/toumain.cpp
@@ -14,23 +14,30 @@ using namespace std;
 
 void addTouHd(char *buf, int n, touheader *th) {
   memset(buf, 0x00, n);
-  string a;
-  stringstream outputv;
+  string a,b,c,d,e,f;
+  stringstream outputv,outputv2,outputv3,outputv4,outputv5;
   outputv << th->seq;
   a = outputv.str();
+  cout<<"size of seq is "<<sizeof(th->seq)<<endl;
+  //a.append(3,'!');
   strncpy(buf,a.c_str(), sizeof(a));
-  outputv << th->mag;
-  a = outputv.str();
-  strncpy(buf,a.c_str(), sizeof(a));
-  outputv << th->ack_seq;
-  a = outputv.str();
-  strncpy(buf,a.c_str(), sizeof(a));
-  outputv << th->flags;
-  a = outputv.str();
-  strncat(buf,a.c_str(), sizeof(a));
-  outputv << th->wnd;
-  a = outputv.str();
-  strncat(buf,a.c_str(), sizeof(a));
+  outputv2 << th->mag;
+  b = outputv2.str();
+  //b.append(3,'!');
+  strncat(buf,b.c_str(), sizeof(a));
+  outputv3 << th->ack_seq;
+  c = outputv3.str();
+  //c.append(3,'!');
+  strncat(buf,c.c_str(), sizeof(a));
+  //outputv4 << th->flags;
+  //cout<<"size of flags is "<<sizeof(th->flags	)<<endl;
+  d = outputv4.str();
+  //d.append(3,'!');
+  strncat(buf,d.c_str(), sizeof(a));
+  outputv5 << th->wnd;
+  e = outputv5.str();
+  //e.append(3,'!');
+  strncat(buf,e.c_str(), sizeof(a));
 };
 
 class toumain {
@@ -38,66 +45,61 @@ class toumain {
 	public :
 	unsigned long test;
 	touheader t;
-	//int tou_socket(void);
-	//int tou_accept();
-	//int tou_bind();
-	//int tou_listen();
-	int tou_send();
-	int tou_recv();
 	int tou_close();
 	struct sockaddr_in socket1;
 	struct sockaddr_in socket2;
 	struct sockaddr_in socket3;
 	int sd;
 	int sd2;
-	char buf[50],buf1[50];
-	/*
+	//char *buf;
+	char buf1[50];
+	//int r = initsoc(socket1);	
 	
-	int initsoc(struct sockaddr_in &socket) {
-	memset(&socket,0,sizeof(socket));
-	socket.sin_family = AF_INET;
-	socket.sin_addr.s_addr=htonl(INADDR_ANY);	
-	//return socket;
-	return 0;	
-	}
-	
-	int r = initsoc(socket1);	
-	*/
 	int tou_socket() {
 	sd = socket(AF_INET,SOCK_DGRAM,0);
 	return sd;
 	}
 	
 	int tou_bind() {
-	int rv;
-	rv = bind(sd,(struct sockaddr*) &socket1,sizeof socket1);
-	return rv;
+		int rv;
+		rv = bind(sd,(struct sockaddr*) &socket1,sizeof socket1);
+		return rv;
 	}
 	
-	int tou_listen() { 
-	int rv;
-	rv = listen(sd,1);
-	}
-	/*
-	int tou_accept() {
-	int rv;
-	rv = recvfrom(sd, buf1, 50, 0, (struct sockaddr *)&socket3, sizeof(struct sockaddr));
-	return rv;
-	}
-	*/
 	int tou_connect() {
-	int rv;
-	t.seq = (u_long)100;
-	t.flags = t.flags | TOU_SYN | TOU_ACK;
-	char msgbuf[101] = {0};
-        addTouHd(msgbuf ,sizeof(msgbuf),&t);
-	cout<<msgbuf <<std::endl;
-        strncat(msgbuf, buf, sizeof(buf));
-	rv = sendto(sd, msgbuf, sizeof(msgbuf), 0, (struct sockaddr*)&socket2, sizeof(struct sockaddr_in));
-	/*printf("%d, %s, %d \n", sd, buf, sizeof(buf));*/
-	perror("talker: sendto");
-	return rv;
-	}//*/
+		int rv;
+		t.seq = rand()%(u_long)65535;
+		t.mag = (u_long)9999;
+		rv = sendto(sd, &t, sizeof(t), 0, (struct sockaddr*)&socket2, 	sizeof(struct sockaddr_in));
+		size_t len = sizeof(sockaddr);
+		rv = recvfrom(sd, &t, sizeof t, 0, (struct sockaddr *)&socket2,	 &len);
+		perror("talker: sendto");
+		cout << "seq no received from server : " << t.seq<<endl;
+		cout << "mag no " << t.mag<<endl;
+		cout << "ack no received from server : " << t.ack_seq<<endl;
+		t.ack_seq = t.seq + 1;
+		rv = sendto(sd, &t, sizeof(t), 0, (struct sockaddr*)&socket2, sizeof(struct sockaddr_in));
+		return true;
+	}
+	
+	int tou_send(char *buf) {
+		ssize_t no;
+		buf[strlen(buf)] = '\0';
+		no = sendto(sd, buf, strlen(buf),0,(struct sockaddr*)&socket2, sizeof(struct sockaddr_in));
+		cout << " Data Sent : " << buf << endl;
+		
+		//buf1 = itoa(no);
+		std::ostringstream buf12;
+		buf12 << no;
+		cout << "no of bytes sent aftr conversion : " << buf12.str() << endl;
+		sendto(sd, buf12, sizeof(buf12),0,(struct sockaddr*)&socket2, sizeof(struct sockaddr_in));
+		t.seq = t.seq + 1;
+		cout << " Sequence no of data sent  : " << t.seq << endl;
+		sendto(sd, &t, sizeof(t),0,(struct sockaddr*)&socket2, sizeof(struct sockaddr_in));
+		memset(buf,0x00,sizeof(buf)+1);
+	
+		return true;
+	}
 };
 	
 int main(int argc, char* argv[])
@@ -106,47 +108,41 @@ int main(int argc, char* argv[])
 	toumain tm;
 	int sd;
 	struct hostent *h;
-	memset(tm.buf, 0, sizeof(tm.buf));
-	strcpy(tm.buf, argv[2]);
-	//tm.buf = argv[2];
+	cout << " Hi ther " <<endl;
+	//memset(tm.buf, 0, strlen(argv[2]));
+	//cout << "Memory set" <<endl;
+	//strcpy(tm.buf, argv[2]);
 	
 	h = gethostbyname(argv[1]);
+
     if(h==NULL) {
       printf("%s: unknown host '%s' \n", argv[0], argv[1]);
-      //exit(1);
     }
 
     printf("%s: sending data to '%s' (IP : %s) \n", argv[0], h->h_name,
     inet_ntoa(*(struct in_addr *)h->h_addr_list[0]));
-      memset(&tm.socket2, 0, sizeof(tm.socket2));
+    memset(&tm.socket2, 0, sizeof(tm.socket2));
    	tm.socket2.sin_family = h->h_addrtype;
    	memcpy((char *) &tm.socket2.sin_addr.s_addr,
     h->h_addr_list[0], h->h_length);
     tm.socket2.sin_port = htons(1500);
-    printf("%d, %d\n", tm.socket2.sin_addr.s_addr, tm.socket2.sin_port);
-	
+    //printf("%d, %d\n", tm.socket2.sin_addr.s_addr, tm.socket2.sin_port);
 	sd = tm.tou_socket();
-	cout<<"Socket"<< sd <<endl;
+	//cout<<"Socket"<< sd <<endl;
 	memset(&tm.socket1,0,sizeof(tm.socket1));
 	tm.socket1.sin_family = AF_INET;
 	tm.socket1.sin_addr.s_addr=htonl(INADDR_ANY);
 	tm.socket1.sin_port = htons(1501);
-/*
-	memset(&tm.socket2,0,sizeof(tm.socket2));
-	tm.socket2.sin_family = AF_INET;
-	tm.socket2.sin_addr.s_addr=htonl(INADDR_ANY);
-*/
-	//sd = tm.tou_bind();
-	cout<<"Bind" << sd <<endl;
-
+	sd = tm.tou_bind();
+	//cout<<"Bind" << sd <<endl;
 	unsigned long len;
 	len = sizeof tm.t;
-	cout<< len <<endl;
+	cout<< "Length of header  : " << len <<endl;
 	sd = tm.tou_connect();
-	cout << sd << endl;
-	//sd = tou_accept();
-	//cout << sd << endl;
-	//sd = tm.tou_listen();
-	//cout<<sd<<endl;
+	//cout <<"Connect : " <<sd << endl;
+	string s1 = inet_ntoa(tm.socket2.sin_addr);
+	string s2 = inet_ntoa(tm.socket1.sin_addr);
+	//cout << " Ips are  : " << s1 << "  "<< s2<<endl ;
+	tm.tou_send(argv[2]);
 	return 0;
 }
--- trunk/toumain2.cpp
+++ trunk/toumain2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<string.h>
+#include<stdlib.h>
 
 
 class toumain {
@@ -15,31 +16,16 @@ class toumain {
 	public :
 	unsigned long test;
 	touheader t;
-	//int tou_socket(void);
-	//int tou_accept();
-	//int tou_bind();
-	//int tou_listen();
-	int tou_send();
-	int tou_recv();
+	socktb s;
 	int tou_close();
 	struct sockaddr_in socket1;
 	struct sockaddr_in socket2;
 	struct sockaddr_in socket3;
 	int sd;
 	int sd2;
-	char buf[50],buf1[50];
-	/*
+	string touhstring;
+	char buf[50],buf1[50],buf3[50],buf4[50],buf5[50];
 	
-	int initsoc(struct sockaddr_in &socket) {
-	memset(&socket,0,sizeof(socket));
-	socket.sin_family = AF_INET;
-	socket.sin_addr.s_addr=htonl(INADDR_ANY);	
-	//return socket;
-	return 0;	
-	}
-	
-	int r = initsoc(socket1);	
-	*/
 	int tou_socket() {
 	sd = socket(AF_INET,SOCK_DGRAM,0);
 	return sd;
@@ -55,30 +41,48 @@ class toumain {
 	int rv;
 	rv = listen(sd,1);
 	}
-	/*
-	?*int tou_accept() {
-	int rv;
-	rv = recvfrom(sd, buf1, 50, 0, (struct sockaddr *)&socket3, sizeof(struct sockaddr));
-	return rv;
-	}
-	*/
+	
 	int tou_accept() {
 	int rv, control=0;
 	size_t len = sizeof(sockaddr);
-	rv = recvfrom(sd, buf, 50, 0, (struct sockaddr *)&socket2, &len);
-	cout << buf <<std::endl;
-        
-
+	rv = recvfrom(sd, &t, sizeof t, 0, (struct sockaddr *)&socket2, &len);
+	cout << "seq no received from client : " << t.seq<<endl;
+	cout << "mag no received from client : " << t.mag<<endl;
+	t.ack_seq = t.seq + 1;
+	t.seq = rand()%(u_long)65530;
+	rv = sendto(sd, &t, sizeof(t), 0, (struct sockaddr*)&socket2, sizeof(struct sockaddr_in));
+	rv = recvfrom(sd, &t, sizeof t, 0, (struct sockaddr *)&socket2, &len);
+	cout << "ack no received for the third handshake : " << t.ack_seq<<endl;
+	len = sizeof t;
+	cout << " tou header size received :  "<< len<<endl; 
+	if (t.ack_seq == t.seq + 1) cout<<"SUCCESS !!!" << endl;
 	return true;
-	}//*/
+	}
+	
+	int tou_recv() {
+	size_t len = sizeof(sockaddr);
+		int no1 = recvfrom(sd, buf, sizeof buf, 0, (struct sockaddr *)&socket2, &len);
+		cout << " Data Received :  " << buf << endl; 
+		cout << " no of bytes received : " << no1 <<endl;
+		recvfrom(sd, buf1, strlen (buf1), 0, (struct sockaddr *)&socket2, &len);
+		cout << buf1 << endl;
+		int no = atoi(buf1);
+		cout << " No of bytes received is :  " << no << endl;
+		recvfrom(sd, &t, sizeof t, 0, (struct sockaddr *)&socket2, &len);
+		cout << " Sequence no received is :  " << t.seq << endl;
+		return true;
+	}	
 };
 	
 int main()
 {
 	toumain tm;
+	touheader t;
+	socktb s;
 	int sd;
-	sd = tm.tou_socket();
-	cout<<"Socket"<< sd <<endl;
+	int sockd;
+	sockd = tm.tou_socket();
+	//cout<<"Socket"<< sockd <<endl;
 	memset(&tm.socket1,0,sizeof(tm.socket1));
 	tm.socket1.sin_family = AF_INET;
 	tm.socket1.sin_addr.s_addr=htonl(INADDR_ANY);
@@ -87,18 +91,22 @@ int main()
 	tm.socket2.sin_family = AF_INET;
 	tm.socket2.sin_addr.s_addr=htonl(INADDR_ANY);
 	sd = tm.tou_bind();
-	cout<<"Bind" << sd <<endl;
+	//cout<<"Bind" << sd <<endl;
 	unsigned long len;
-	len = sizeof tm.t;
-	cout << tm.t.flags <<std::endl;
-	cout<< len <<endl;
-	//sd = tou_connect();
-	//cout << sd << endl;
+	
 	while(1) {
-	sd = tm.tou_accept();
-	cout << sd << endl;
+		sd = tm.tou_accept();
+		if (sd == 1) {
+				s.sockd = sockd;
+				s.dport = tm.socket1.sin_port;
+				s.sport = tm.socket2.sin_port;
+				s.dip = inet_ntoa(tm.socket2.sin_addr);
+				s.sip = inet_ntoa(tm.socket1.sin_addr);
+				}
+	//	cout <<"socket table info : "<<"Sockfd  " << s.sockd<< " ports : "<< ntohs(s.dport)<<" " << ntohs(s.sport) <<" ip : "<< s.sip <<" "<<s.dip << endl ;
+	//cout << sd << endl;
 	cout << "get the msg from client connect() \n";
-	}//sd = tm.tou_listen();
-	//cout<<sd<<endl;
+	tm.tou_recv();
+	}
 	return 0;
 }
