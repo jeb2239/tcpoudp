4e7941ed494b174c7c7b39e1aef2bbc910507a1e
chinmay.gaikwad <chinmay.gaikwad@469fafa2-ad12-11de-a118-c7387b1e4f66>
2009-10-27 04:22:45 +0000

    nomsg

--- trunk/toumain2.cpp
+++ trunk/toumain2.cpp
@@ -1,3 +1,11 @@
+/***************************************************
+
+
+				TOU SERVER
+				
+
+***************************************************/
+
 #include<stdio.h>
 #include <iostream>
 using namespace std;
@@ -13,17 +21,23 @@ using namespace std;
 
 //-------------------  TOUMAIN CLASS ---------------
 boost::mutex soctabmutex;
-class toumain {
+class touMain {
  	
 	public :
 	unsigned long test;
-	toupkg tp;
+	touPkg tp;
 	//touheader t;
-	socktb s;
-	int tou_close();
+	sockTb s;
+	
+	//TODO : Remove when implemented
+	int tou_close(); 
+	
+	// Define structures
 	struct sockaddr_in socket1;
 	struct sockaddr_in socket2;
 	struct sockaddr_in socket3;
+	
+	// Socket Descriptors
 	int sd;
 	int sd2;
 	int yes;
@@ -31,7 +45,9 @@ class toumain {
 	char buf[50],buf1[50],buf3[50],buf4[50],buf5[50];
 	timerMng timermng;
 	
-	void converttobyteorder(toupkg tp) {
+	// Byte order conversions
+	
+	void convertToByteOrder(touPkg tp) {
 	tp.t.seq = htonl(tp.t.seq);
     tp.t.mag = htonl(tp.t.mag);
     tp.t.ack_seq = htonl(tp.t.ack_seq);
@@ -40,65 +56,74 @@ class toumain {
     
     }
 	
-	void convertfrombyteorder(toupkg tp) {
+	void convertFromByteOrder(touPkg tp) {
 	tp.t.seq = ntohl(tp.t.seq);
     tp.t.mag = ntohl(tp.t.seq);
     tp.t.ack_seq = ntohl(tp.t.seq);
     }
     
-	int tou_socket() {
+    
+    // ------------------------------CREATE ----------------------------------
+    
+	int touSocket() {
 	sd = socket(AF_INET,SOCK_DGRAM,0);
 	setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 	return sd;
 	
 	}
 	
-	int tou_bind() {
+	//----------------------------BIND ---------------------------------------
+	
+	int touBind() {
 	int rv;
 	rv = bind(sd,(struct sockaddr*) &socket1,sizeof socket1);
 	return rv;
 	}
 	
-	int tou_listen() { 
+	//-----------------------------LISTEN -----------------------------------
+	//TODO : Modify this 
+	
+	int touListen() { 
 	int rv;
 	rv = listen(sd,1);
 	}
 	
 	
-	//-------------------ACCEPT --------------------- 
+	//---------------------------ACCEPT --------------------- 
 	
-	int tou_accept() {
+	int touAccept() {
 		int rv, control=0, flagforsyn = 1;
 		size_t len = sizeof(sockaddr);
-		converttobyteorder(tp);
-		cout << endl << " INSIDE TOU_ACCEPT () " <<endl;
+		convertToByteOrder(tp);
+		cout << endl << " INSIDE TOUACCEPT () " <<endl;
+		
 		// receive first handshake
 		
 		rv = recvfrom(sd, &tp, sizeof tp, 0, (struct sockaddr *)&socket2, &len);
 		cout << "seq no received from client : " << tp.t.seq<<endl;
-		cout << "mag no received from client : " << tp.t.mag<<endl;
 		
 		//send syn ack
+		
 		tp.t.ack_seq = tp.t.seq + 1;
 		cout << "ack no sent to client : " << tp.t.ack_seq<<endl;
 		tp.t.seq = rand()%(u_long)65530;
 		tp.t.syn = 1;
 		tp.t.ack = 1;
-		converttobyteorder(tp);
+		convertToByteOrder(tp);
 		
-			sleep(3);
+		//Code for testing begin
+		/*	sleep(3);
 		
 				rv = sendto(sd, &tp, sizeof(tp), 0, (struct sockaddr*)&socket2, sizeof(struct sockaddr_in));
-		
+		*/
+		//Code for testing end
 		
 		//recv third handshake
 		
 		rv = recvfrom(sd, &tp, sizeof tp, 0, (struct sockaddr *)&socket2, &len);
-		convertfrombyteorder(tp);
+		convertFromByteOrder(tp);
 		cout << "ack no received for the third handshake : " << tp.t.ack_seq<<endl;
-		len = sizeof tp.t;
-		cout << " tou header size received :  "<< len<<endl; 
-		cout << endl << " LEAVING TOU_ACCEPT () " <<endl;
+		cout << endl << " LEAVING TOUACCEPT () " <<endl;
 		
 		if (tp.t.ack_seq == tp.t.seq + 1) cout<<"SUCCESS !!!" << endl;
 		return true;
@@ -107,51 +132,52 @@ class toumain {
 	
 	//-----------------	RECEIVE  --------------------------
 	
-	int tou_recv() {
+	int touRecv() {
+	
 	memset(tp.buf,0,50);
 	char buf12[10];
 	int no1;
 	memset(buf12,0,10);
 	size_t len = sizeof(sockaddr);
-	cout << endl << " INSIDE TOU_RECV () " <<endl;
+	cout << endl << " INSIDE TOURECV () " <<endl;
 		
 		//timermng.add(1,3333,4000,101);
+		
+		//Recv data
+		
 		no1 = recvfrom(sd, &tp, sizeof tp, 0, (struct sockaddr *)&socket2, &len);
 		cout << " Data Received :  " << tp.buf << endl; 
 		cout << " no of bytes received : " << no1 <<endl;
-		if(tp.buf == "q" )
-		{
-			cout << " Connection closed by client "<< endl;
-			close(sd);
-			return true;
-		}
-		recvfrom(sd, buf12, sizeof (buf12), 0, (struct sockaddr *)&socket2, &len);
-		//cout << buf12 << endl;
-		int no = atoi(buf12);
-		cout << " No of bytes received is :  " << no << endl;
-		//recvfrom(sd, &t, sizeof t, 0, (struct sockaddr *)&socket2, &len);
-		convertfrombyteorder(tp);
+		
+		convertFromByteOrder(tp);
 		cout << " Sequence no received is :  " << tp.t.seq << endl;
-		if(no1 == no) {
+		
+		//TODO : Put condition to check the data here
+		
+		//Send ACK
+		{
 			tp.t.ack_seq = tp.t.seq + 1;
-			converttobyteorder(tp);
+			convertToByteOrder(tp);
 			sendto(sd, &tp, sizeof(tp), 0, (struct sockaddr*)&socket2, sizeof(struct sockaddr_in));
 			cout << " Ack sent " <<endl; 
 		}
-		return true;
+		return no1;
 	}	
 };
 	
 int main()
 {
-	toumain tm;
-	touheader t;
-	socktb s;
+	touMain tm;
+	touHeader t;
+	sockTb s;
 	int sd,bytes_recieved;
 	int sockd;
-	tm.yes = 1; 
-	sockd = tm.tou_socket();
+	tm.yes = 1;
 	
+	//CREATE 
+	sockd = tm.touSocket();
+	
+	//Set socket structures
 	memset(&tm.socket1,0,sizeof(tm.socket1));
 	tm.socket1.sin_family = AF_INET;
 	tm.socket1.sin_addr.s_addr=htonl(INADDR_ANY);
@@ -160,37 +186,37 @@ int main()
 	tm.socket2.sin_family = AF_INET;
 	tm.socket2.sin_addr.s_addr=htonl(INADDR_ANY);
 	
-	sd = tm.tou_bind();
+	//BIND
+	sd = tm.touBind();
 	unsigned long len;
 	
 	while(1) {
-		sd = tm.tou_accept();
+		sd = tm.touAccept();
 		if (sd == 1) {
-		boost::mutex::scoped_lock lock(soctabmutex);
+		
+		
+	//SOCKET TABLE INFO 
+			boost::mutex::scoped_lock lock(soctabmutex);
 			s.sockd = sockd;
 			s.dport = tm.socket1.sin_port;
 			s.sport = tm.socket2.sin_port;
 			s.dip = inet_ntoa(tm.socket2.sin_addr);
 			s.sip = inet_ntoa(tm.socket1.sin_addr);
+			
 				
-			cout <<"socket table info : "<<endl<<"Sockfd : " << s.sockd<< endl<<"_________________________"<<endl<<" ports : "<< ntohs(s.dport)<<" " << ntohs(s.sport) <<endl<<" ip : "<< s.sip <<" "<<s.dip << endl ;
+			cout <<"socket table info : "<<endl
+			cout <<"Sockfd : " << s.sockd<< endl
+			cout <<"-------------------"<<endl
+			cout <<"ports : "<< ntohs(s.dport) <<" " << ntohs(s.sport) <<endl
+			cout <<" ip : "<< s.sip <<" "<<s.dip << endl ;
 			}
 			while(1) 
-		{
+			{
 			cout << "get the msg from client connect() \n";	
-			
-			tm.tou_recv();
-			/*if (strcmp(recv_data , "q") == 0 || strcmp(recv_data , "Q") == 0)
-              {
-                close(connected);
-                break;
-              }
+			tm.touRecv();
+			  fflush(stdout);
 
-              else 
-              printf("\n RECIEVED DATA = %s " , recv_data);*/
-              fflush(stdout);
-
-		}
+			}
 	
 	}
 	return 0;
